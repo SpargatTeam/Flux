7dbc1d9d1efc689fa3ef62ccba84510a9a5c57d3
ComicalSGT <matesstelianioan@gmail.com>
2025-01-01 20:10:00 +0200

    update

--- setup.sh
+++ setup.sh
@@ -1,16 +1,29 @@
-# linux and windows dependencies
-sudo apt-get install g++ libc6-dev libx11-dev build-essential mingw-w64 git clang o64-clang++ -y
-# libs we use 
-sudo apt install libcurl4-openssl-dev libfltk1.3-dev -y
-# android dependencies
+#!/bin/bash
+
+# Instalare dependențe pentru Linux și Windows
+sudo apt-get update
+sudo apt-get install -y g++ libc6-dev libx11-dev build-essential mingw-w64 git clang o64-clang++
+
+# Instalare librării necesare
+sudo apt-get install -y libcurl4-openssl-dev libfltk1.3-dev
+
+# Dependențe pentru Android
 wget https://dl.google.com/android/repository/android-ndk-r27c-linux.zip
 unzip android-ndk-r27c-linux.zip
 export PATH=$PATH:$(pwd)/android-ndk-r27c/toolchains/llvm/prebuilt/linux-x86_64/bin
-sudo apt-get install openjdk-21-jdk kotlin -y
-# macos depencies
+sudo apt-get install -y openjdk-21-jdk kotlin
+
+# Dependențe pentru macOS
 git clone https://github.com/tpoechtrager/osxcross.git
-mkdir -p tarballs
-cp $(pwd)/libs/MacOSX10.11.sdk.tar.xz $(pwd)/osxcross/tarballs/ 
+mkdir -p osxcross/tarballs
+cp $(pwd)/libs/MacOSX10.11.sdk.tar.xz osxcross/tarballs/
 sudo bash osxcross/tools/get_dependencies.sh
-SDK_VERSION=10.11 ./osxcross/build.sh
-export PATH=$PATH::$(pwd)/osxcross/target/binexport PATH=$PATH:$(pwd)/android-ndk-r27c/toolchains/llvm/prebuilt/linux-x86_64/bin
+cd osxcross
+SDK_VERSION=10.11 ./build.sh
+cd ..
+export PATH=$PATH:$(pwd)/osxcross/target/bin
+
+# Dependențe pentru Android (adăugare repetată pentru siguranță)
+export PATH=$PATH:$(pwd)/android-ndk-r27c/toolchains/llvm/prebuilt/linux-x86_64/bin
+
+echo "Toate dependențele au fost instalate și configurate cu succes!"

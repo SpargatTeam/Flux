b7c22894ead5d324b9d5fe12e98f8316eec8b751
ComicalSGT <matesstelianioan@gmail.com>
2025-01-01 20:07:40 +0200

    update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # linux and windows dependencies
-sudo apt-get install g++ libc6-dev libx11-dev build-essential mingw-w64 git clang -y
+sudo apt-get install g++ libc6-dev libx11-dev build-essential mingw-w64 git clang o64-clang++ -y
 # libs we use 
 sudo apt install libcurl4-openssl-dev libfltk1.3-dev -y
 # android dependencies
